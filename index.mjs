7224dc3be0c3b312f19f7a6caf6f73a73542bd91
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-05-02 16:15:38 +0330

    updat web scraping project

--- index.mjs
+++ index.mjs
@@ -1,11 +1,27 @@
+import express from "express";
 import { load } from "cheerio";
-const response = await fetch("https://en.wikipedia.org/wiki/Node.js");
-const data = await response.text();
 
-const $ = load(data);
+const app = express();
 
-const summary = $(
-  "#mw-content-text > div.mw-parser-output > table + p:first-of-type"
-).text();
+app.get("/topic/:topic", async (req, res) => {
 
-console.log("Wikipedia summar:", summary);
+  const response = await fetch(
+    `https://en.wikipedia.org/wiki/${req.params.topic}`
+  );
+  const data = await response.text();
+
+  const $ = load(data);
+
+  const summary = $("#mw-content-text > div.mw-parser-output > table").text();
+
+  if (summary.length < 1) {
+    res.status(404).send(`Topic ${req.params.topic} not found`);
+    return;
+  }
+
+  res.send(summary);
+});
+
+app.listen(3000, () => {
+  console.log("wikipediaSummary is rounning on port 3000");
+});
